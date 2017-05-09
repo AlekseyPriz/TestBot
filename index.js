15d727effbb0bbe916e42b17d2a8fa6f9ac99f21
AlekseyPriz <aleksey.priz@gmail.com>
2017-05-09 15:05:27 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var dialog = require( './dialog' );
 const vk = new (require( 'vk-io' ));
 
 
-vk.setToken('6ae7b294baa55e8b74738e546aa3a7fdd1300735f5a0c4d1ccfeef31ab6ddb715c68e6f13b71ea4e84039');
+vk.setToken('token');
 
     vk.longpoll.start()
         .then(() => {
